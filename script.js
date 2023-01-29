3e83c4ce2e96efff088386f5cc1be9ca39db013c
Ravshanbek <123853445+Halkulov@users.noreply.github.com>
2023-01-29 19:05:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// window.addEventListener("load", function (e) {
+//   var imgs = document.querySelectorAll(".slider_img img"); // [0,1,2,3]
+//   var next = document.querySelector(".btn-next");
+//   var prev = document.querySelector(".btn-prev");
+//   var i = 0;
+
+//   next.addEventListener("click", function (e) {
+//     imgs[i].classList.remove('active')
+//     i++
+//     if(i > imgs.length - 1){
+//         i = 0
+//     }
+//     imgs[i].classList.add('active')
+//   });
+
+
+
+// prev.addEventListener("click", function (e) {
+//   imgs[i].classList.remove('active')
+//   i--
+//   if(i < 0 ){
+//       i = imgs.length -1
+//   }
+//   imgs[i].classList.add('active')
+// });
+
+// });
